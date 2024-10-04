41f5398c193179b07fbceb10a7d044c30e136ba4
hastes <rom541997@gmail.com>
2024-10-04 14:20:20 +0700

    deploy(modes)

--- apps/web-tools/app/helpers/constants.ts
+++ apps/web-tools/app/helpers/constants.ts
@@ -1,4 +1,18 @@
 import { DEXTRADE_BASE_URLS } from 'dex-helpers';
 
-export const DEXTRADE_BASE_URL = DEXTRADE_BASE_URLS.prod;
+const getUrl = () => {
+  const currentMode = import.meta.env.MODE;
+  switch (currentMode) {
+    case 'development':
+      return DEXTRADE_BASE_URLS.dev;
+    case 'staging':
+      return DEXTRADE_BASE_URLS.stage;
+    case 'production':
+      return DEXTRADE_BASE_URLS.prod;
+    default:
+      return DEXTRADE_BASE_URLS.dev;
+  }
+};
+
+export const DEXTRADE_BASE_URL = getUrl();
 export const DEXTRADE_HOST = DEXTRADE_BASE_URL.replace('https://', '');
