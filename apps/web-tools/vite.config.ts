ba345729a73a3ab26b69075e01d64f417f676070
hastes <rom541997@gmail.com>
2024-10-04 14:31:45 +0700

    deploy(polyfill)

--- apps/web-tools/vite.config.ts
+++ apps/web-tools/vite.config.ts
@@ -1,10 +1,22 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import wasm from 'vite-plugin-wasm';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), wasm()],
+  plugins: [
+    react(),
+    wasm(),
+    nodePolyfills({
+      include: ['buffer', 'process', 'crypto'],
+      globals: {
+        Buffer: true,
+        global: true,
+        process: true,
+      },
+    }),
+  ],
   optimizeDeps: {
     esbuildOptions: {
       // Node.js global to browser globalThis
@@ -15,10 +27,5 @@ export default defineConfig({
   },
   build: {
     target: 'esnext',
-  },
-  resolve: {
-    alias: {
-      crypto: 'crypto-browserify',
-    },
   },
 });
