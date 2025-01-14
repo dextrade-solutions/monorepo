caaf36e0fee9f748302d709202e090a972d2591e
hastes <rom541997@gmail.com>
2025-01-14 17:02:10 +0700

    feat(dex-preloader)

--- apps/web-tools/app/helpers/web3-client-configuration.ts
+++ apps/web-tools/app/helpers/web3-client-configuration.ts
@@ -11,8 +11,8 @@ import {
 } from 'wagmi/chains';
 import { coinbaseWallet, metaMask } from 'wagmi/connectors';
 
-import { walletConnect } from '../../ui/helpers/utils/wc-connector';
 import { trustWalletConnect } from '../../ui/helpers/utils/wagmi-connectors/trust';
+import { walletConnect } from '../../ui/helpers/utils/wc-connector';
 
 // 1. Get projectId at https://cloud.walletconnect.com
 const projectId = '1ee56a25a2dad471b92feb59898b7aa6';
